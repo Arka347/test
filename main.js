c30b1865a3c806e9d5fd24f0d89088f7b2d832fc
Arka347 <82458894+Arka347@users.noreply.github.com>
2021-07-01 21:41:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+ var names = ["Ahana bhattacharjee", "Arka bhattacharjee", "Saumendu bhattacharjee", "Suchandra bhattacharjee"];
+ 
+ var i = 0;
+ function update(){
+     document.getElementById(names).innerHTML = names;
+     i++;
+ }
